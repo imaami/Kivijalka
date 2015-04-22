cf61132171367852f8f3863a107c88b330df556d
Juuso Alasuutari <juuso.alasuutari@koneetkiertoon.fi>
2015-04-22 16:11:05 +0300

    Use -std=gnu11 when compiling C code.

--- server/ws/build.sh
+++ server/ws/build.sh
@@ -4,9 +4,12 @@ if [[ -f /opt/qt54/bin/qt54-env.sh ]]; then
   source /opt/qt54/bin/qt54-env.sh
 fi
 
-if [[ ! -f Makefile ]]; then
-  if qmake; then
-    sed -i 's|-pipe -O2|-march=native -pipe -O3|' Makefile
-    make
-  fi
+if [[ -f Makefile ]]; then
+  mv Makefile Makefile.old
+fi
+
+if qmake; then
+  sed -ri 's|(CFLAGS.*)-pipe -O2|\1-march=native -pipe -O3 -std=gnu11|' Makefile
+  sed -ri 's|(CXXFLAGS.*)-pipe -O2|\1-march=native -pipe -O3|' Makefile
+  make
 fi
