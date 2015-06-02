c5385a738b32e6f2cebb3d6b2ab7058385a327e1
Juuso Alasuutari <juuso.alasuutari@koneetkiertoon.fi>
2015-06-02 22:20:32 +0300

    WSServer: Continuing work on ImgThread::run().

--- server/ws/imgthread.cpp
+++ server/ws/imgthread.cpp
@@ -21,31 +21,52 @@ void ImgThread::run()
 {
 	for (;;) {
 		if (!sem_wait (&process_sem)) {
-			unsigned int update_flags = 0;
-			img_data_t *imd;
-			if (img_file_steal_data (&capture_file, &imd)) {
-				if (imd) {
-					if (img_load_data (&img, 1, imd->data, imd->size)) {
-						update_flags |= 1;
-						std::printf ("%s: loaded capture data\n", __func__);
-					} else {
-						std::fprintf (stderr, "%s: img_load_data failed\n", __func__);
-					}
-					img_data_free (imd);
-				} else {
-					std::printf ("%s: no capture data available\n", __func__);
+			img_data_t *capture_data, *banner_data;
+
+			if (!img_file_steal_data (&capture_file,
+			                          &capture_data)) {
+				capture_data = NULL;
+			}
+
+			if (!img_file_steal_data (&banner_file,
+			                          &banner_data)) {
+				banner_data = NULL;
+			}
+
+			if (capture_data) {
+				if (img_load_data (&img, 1,
+				                   capture_data->data,
+				                   capture_data->size)) {
+					std::printf ("%s: loaded capture data\n", __func__);
 				}
-			} else {
-				std::fprintf (stderr, "%s: img_file_steal_data failed\n", __func__);
-			}
-			imd = NULL;
-			switch (update_flags) {
-			case 0:
-				std::printf ("%s: nothing to update\n", __func__);
-				continue;
-			default:
+
+				img_data_free (capture_data);
+				capture_data = NULL;
+
+				if (banner_data) {
+					goto have_banner_update;
+				} else {
+					goto do_render_update;
+				}
+
+			} else if (banner_data) {
+			have_banner_update:
+				if (img_load_data (&img, 2,
+				                   banner_data->data,
+				                   banner_data->size)) {
+					std::printf ("%s: loaded banner data\n", __func__);
+				}
+
+				img_data_free (banner_data);
+				banner_data = NULL;
+
+			do_render_update:
 				(void) img_file_post (&output_file);
+
+			} else {
+				std::printf ("%s: nothing to update\n", __func__);
 			}
+
 		} else if (errno != EINTR) {
 			std::fprintf (stderr, "%s: sem_wait failed: %s\n",
 			              __func__, std::strerror (errno));
