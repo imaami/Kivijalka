21be0813fa8712a32952c16bb1985387820613ea
Juuso Alasuutari <juuso.alasuutari@koneetkiertoon.fi>
2015-06-02 17:24:28 +0300

    WSServer: Working on ImgThread::run().

--- server/ws/imgthread.cpp
+++ server/ws/imgthread.cpp
@@ -1,5 +1,6 @@
 #include "imgthread.h"
 #include "global.h"
+#include "img_file.h"
 #include "img.h"
 
 #include <cstdlib>
@@ -20,8 +21,29 @@ void ImgThread::run()
 {
 	for (;;) {
 		if (!sem_wait (&process_sem)) {
-			if (img_load_data (&img, 1, capture_file.data->data, capture_file.data->size)) {
-				std::printf ("loaded capture data\n");
+			unsigned int update_flags = 0;
+			img_data_t *imd;
+			if (img_file_steal_data (&capture_file, &imd)) {
+				if (imd) {
+					if (img_load_data (&img, 1, imd->data, imd->size)) {
+						update_flags |= 1;
+						std::printf ("%s: loaded capture data\n", __func__);
+					} else {
+						std::fprintf (stderr, "%s: img_load_data failed\n", __func__);
+					}
+					img_data_free (imd);
+				} else {
+					std::printf ("%s: no capture data available\n", __func__);
+				}
+			} else {
+				std::fprintf (stderr, "%s: img_file_steal_data failed\n", __func__);
+			}
+			imd = NULL;
+			switch (update_flags) {
+			case 0:
+				std::printf ("%s: nothing to update\n", __func__);
+				continue;
+			default:
 				(void) img_file_post (&output_file);
 			}
 		} else if (errno != EINTR) {
