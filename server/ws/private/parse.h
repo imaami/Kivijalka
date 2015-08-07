06edbf67b8ebb4c05fbc5274ea1b6319da5dddb2
Juuso Alasuutari <juuso.alasuutari@koneetkiertoon.fi>
2015-08-07 14:08:06 +0300

    WSServer: Add the remaining integer parsing functions to private/parse.h           save for the one for int8_t. Also improve test-parse.c.

--- server/ws/private/parse.h
+++ server/ws/private/parse.h
@@ -11,6 +11,257 @@
 
 __attribute__((always_inline))
 static inline bool
+_parse_u8 (char         *str,
+           unsigned int *pos,
+           uint8_t      *dest)
+{
+	uint8_t v;
+	unsigned int p = *pos;
+	unsigned int c = str[p];
+
+	switch (c) {
+	case '0':
+		v = 0;
+		++p;
+		break;
+
+	case '1':
+		v = 1;
+		c = str[++p];
+		switch (c) {
+		case '0' ... '9':
+			v = (v << 1) + (v << 3) + (c - '0');
+			c = str[++p];
+			switch (c) {
+			case '0' ... '9':
+				goto _must_end_u8;
+			}
+		}
+		break;
+
+	case '2':
+		v = 2;
+		c = str[++p];
+		switch (c) {
+		case '0' ... '4':
+			v = (v << 1) + (v << 3) + (c - '0');
+			c = str[++p];
+			switch (c) {
+			case '0' ... '9':
+				goto _must_end_u8;
+			}
+			break;
+
+		case '5':
+			v = (v << 1) + (v << 3) + (c - '0');
+			c = str[++p];
+			switch (c) {
+			case '0' ... '5':
+				goto _must_end_u8;
+			case '6' ... '9':
+				goto _overflow_u8;
+			}
+			break;
+
+		case '6' ... '9':
+			goto _must_end_u8;
+		}
+		break;
+
+	case '3' ... '9':
+		v = c - '0';
+		c = str[++p];
+		switch (c) {
+		case '0' ... '9':
+		_must_end_u8:
+			v = (v << 1) + (v << 3) + (c - '0');
+			c = str[++p];
+			switch (c) {
+			case '0' ... '9':
+			_overflow_u8:
+				*dest = UINT8_MAX;
+				return false;
+			}
+		}
+		break;
+
+	default:
+	_error_u8:
+		*dest = 0;
+		return false;
+	}
+
+	*pos = p;
+	*dest = v;
+	return true;
+}
+
+__attribute__((always_inline))
+static inline bool
+_parse_u16 (char         *str,
+            unsigned int *pos,
+            uint16_t     *dest)
+{
+	uint16_t v;
+	unsigned int i, p = *pos;
+	unsigned int c = str[p];
+
+	switch (c) {
+	case '0':
+		*pos = p + 1;
+		*dest = 0;
+		return true;
+
+	case '1' ... '9':
+		v = c - '0';
+		i = p + 1;
+		for (;;) {
+			switch ((c = str[i])) {
+			case '0' ... '9':
+				v = (v << 1) + (v << 3) + (c - '0');
+
+				if (++i - p < 4) {
+					continue;
+				}
+
+				switch ((c = str[i])) {
+				case '0' ... '9':
+					if (v > 6553
+					    || (v == 6553 && c > '5')) {
+						goto _overflow_u16;
+					}
+
+					v = (v << 1) + (v << 3) + (c - '0');
+
+					switch ((c = str[++i])) {
+					case '0' ... '9':
+					_overflow_u16:
+						*dest = UINT16_MAX;
+						return false;
+					}
+				}
+			}
+
+			break;
+		}
+
+		*pos = i;
+		*dest = v;
+		return true;
+	}
+
+	*dest = 0;
+	return false;
+}
+
+__attribute__((always_inline))
+static inline bool
+_parse_i16 (char         *str,
+            unsigned int *pos,
+            int16_t      *dest)
+{
+	int16_t v;
+	unsigned int i, p = *pos;
+	unsigned int c = str[p];
+
+	switch (c) {
+	case '-':
+		switch ((c = str[++p])) {
+		case '0':
+			goto _zero;
+
+		case '1' ... '9':
+			v = 0 - (int16_t) (c - '0');
+			i = p + 1;
+			for (;;) {
+				switch ((c = str[i])) {
+				case '0' ... '9':
+					v *= 10;
+					v -= (c - '0');
+
+					if (++i - p < 4) {
+						continue;
+					}
+
+					switch ((c = str[i])) {
+					case '0' ... '9':
+						if (v < -3276
+						    || (v == -3276 && c > '8')) {
+							goto _underflow_i16;
+						}
+
+						v *= 10;
+						v -= (c - '0');
+
+						switch ((c = str[++i])) {
+						case '0' ... '9':
+						_underflow_i16:
+							*dest = INT16_MIN;
+							return false;
+						}
+					}
+				}
+
+				break;
+			}
+
+			*pos = i;
+			*dest = v;
+			return true;
+		}
+
+		break;
+
+	case '0':
+	_zero:
+		*pos = p + 1;
+		*dest = 0;
+		return true;
+
+	case '1' ... '9':
+		v = c - '0';
+		i = p + 1;
+		for (;;) {
+			switch ((c = str[i])) {
+			case '0' ... '9':
+				v = (v << 1) + (v << 3) + (c - '0');
+
+				if (++i - p < 4) {
+					continue;
+				}
+
+				switch ((c = str[i])) {
+				case '0' ... '9':
+					if (v > 3276
+					    || (v == 3276 && c > '7')) {
+						goto _overflow_i16;
+					}
+
+					v = (v << 1) + (v << 3) + (c - '0');
+
+					switch ((c = str[++i])) {
+					case '0' ... '9':
+					_overflow_i16:
+						*dest = INT16_MAX;
+						return false;
+					}
+				}
+			}
+
+			break;
+		}
+
+		*pos = i;
+		*dest = v;
+		return true;
+	}
+
+	*dest = 0;
+	return false;
+}
+
+__attribute__((always_inline))
+static inline bool
 _parse_u32 (char         *str,
             unsigned int *pos,
             uint32_t     *dest)
@@ -156,6 +407,173 @@ _parse_i32 (char         *str,
 					case '0' ... '9':
 					_overflow_i32:
 						*dest = INT32_MAX;
+						return false;
+					}
+				}
+			}
+
+			break;
+		}
+
+		*pos = i;
+		*dest = v;
+		return true;
+	}
+
+	*dest = 0;
+	return false;
+}
+
+__attribute__((always_inline))
+static inline bool
+_parse_u64 (char         *str,
+            unsigned int *pos,
+            uint64_t     *dest)
+{
+	uint64_t v;
+	unsigned int i, p = *pos;
+	unsigned int c = str[p];
+
+	switch (c) {
+	case '0':
+		*pos = p + 1;
+		*dest = 0;
+		return true;
+
+	case '1' ... '9':
+		v = c - '0';
+		i = p + 1;
+		for (;;) {
+			switch ((c = str[i])) {
+			case '0' ... '9':
+				v = (v << 1) + (v << 3) + (c - '0');
+
+				if (++i - p < 19) {
+					continue;
+				}
+
+				switch ((c = str[i])) {
+				case '0' ... '9':
+					if (v > 1844674407370955161
+					    || (v == 1844674407370955161
+					        && c > '5')) {
+						goto _overflow_u64;
+					}
+
+					v = (v << 1) + (v << 3) + (c - '0');
+
+					switch ((c = str[++i])) {
+					case '0' ... '9':
+					_overflow_u64:
+						*dest = UINT64_MAX;
+						return false;
+					}
+				}
+			}
+
+			break;
+		}
+
+		*pos = i;
+		*dest = v;
+		return true;
+	}
+
+	*dest = 0;
+	return false;
+}
+
+__attribute__((always_inline))
+static inline bool
+_parse_i64 (char         *str,
+            unsigned int *pos,
+            int64_t      *dest)
+{
+	int64_t v;
+	unsigned int i, p = *pos;
+	unsigned int c = str[p];
+
+	switch (c) {
+	case '-':
+		switch ((c = str[++p])) {
+		case '0':
+			goto _zero;
+
+		case '1' ... '9':
+			v = 0 - (int64_t) (c - '0');
+			i = p + 1;
+			for (;;) {
+				switch ((c = str[i])) {
+				case '0' ... '9':
+					v *= 10;
+					v -= (c - '0');
+
+					if (++i - p < 18) {
+						continue;
+					}
+
+					switch ((c = str[i])) {
+					case '0' ... '9':
+						if (v < -922337203685477580
+						    || (v == -922337203685477580
+						        && c > '8')) {
+							goto _underflow_i64;
+						}
+
+						v *= 10;
+						v -= (c - '0');
+
+						switch ((c = str[++i])) {
+						case '0' ... '9':
+						_underflow_i64:
+							*dest = INT64_MIN;
+							return false;
+						}
+					}
+				}
+
+				break;
+			}
+
+			*pos = i;
+			*dest = v;
+			return true;
+		}
+
+		break;
+
+	case '0':
+	_zero:
+		*pos = p + 1;
+		*dest = 0;
+		return true;
+
+	case '1' ... '9':
+		v = c - '0';
+		i = p + 1;
+		for (;;) {
+			switch ((c = str[i])) {
+			case '0' ... '9':
+				v = (v << 1) + (v << 3) + (c - '0');
+
+				if (++i - p < 18) {
+					continue;
+				}
+
+				switch ((c = str[i])) {
+				case '0' ... '9':
+					if (v > 922337203685477580
+					    || (v == 922337203685477580
+					        && c > '7')) {
+						goto _overflow_i64;
+					}
+
+					v = (v << 1) + (v << 3) + (c - '0');
+
+					switch ((c = str[++i])) {
+					case '0' ... '9':
+					_overflow_i64:
+						*dest = INT64_MAX;
 						return false;
 					}
 				}
--- server/ws/test-parse.c
+++ server/ws/test-parse.c
@@ -5,47 +5,151 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <inttypes.h>
 
 int
 main (int    argc,
       char **argv)
 {
-	unsigned int i, pos;
-	char *ptr;
-	union {
-		uint32_t u;
-		int32_t  i;
-	} v;
-	for (i = 1; i < argc; ++i) {
-		ptr = argv[i];
-		for (ptr = argv[i]; *ptr; ptr += pos) {
-			printf ("argv[%d]: ", i);
-			pos = 0;
-			if (*ptr == 'u') {
-				printf ("uint32 ");
-				++ptr;
-				if (!_parse_u32 (ptr, &pos, &v.u)) {
-					printf ("%s\n",
-					        (v.u == UINT32_MAX)
-					        ? "overflow"
-					        : "error");
-					return EXIT_FAILURE;
+	if (argc < 2 || !argv) {
+		return EXIT_FAILURE;
+	}
+
+	puts ("arg  pos  type  value");
+
+	unsigned int i = 1;
+	while (i < argc) {
+		char *ptr = argv[i];
+		unsigned int pos = 0;
+		while (ptr[pos]) {
+			if (ptr[pos] == 'u') {
+				if (ptr[pos + 1] == '8'
+				    && ptr[pos + 2] == ':') {
+					pos += 3;
+					uint8_t v;
+					printf ("%3.u  %3.d   u8   ", i, pos);
+					if (!_parse_u8 (ptr, &pos, &v)) {
+						printf ("%s\n",
+							(v == UINT8_MAX)
+							? "overflow"
+							: "error");
+						goto _next_arg;
+					}
+					printf ("%" PRIu8 "\n", v);
+
+				} else if (ptr[pos + 1] == '1'
+				           && ptr[pos + 2] == '6'
+				           && ptr[pos + 3] == ':') {
+					pos += 4;
+					uint16_t v;
+					printf ("%3.u  %3.d  u16   ", i, pos);
+					if (!_parse_u16 (ptr, &pos, &v)) {
+						printf ("%s\n",
+							(v == UINT16_MAX)
+							? "overflow"
+							: "error");
+						goto _next_arg;
+					}
+					printf ("%" PRIu16 "\n", v);
+
+				} else if (ptr[pos + 1] == '3'
+				           && ptr[pos + 2] == '2'
+				           && ptr[pos + 3] == ':') {
+					pos += 4;
+					uint32_t v;
+					printf ("%3.u  %3.d  u32   ", i, pos);
+					if (!_parse_u32 (ptr, &pos, &v)) {
+						printf ("%s\n",
+							(v == UINT32_MAX)
+							? "overflow"
+							: "error");
+						goto _next_arg;
+					}
+					printf ("%" PRIu32 "\n", v);
+
+				} else if (ptr[pos + 1] == '6'
+				           && ptr[pos + 2] == '4'
+				           && ptr[pos + 3] == ':') {
+					pos += 4;
+					uint64_t v;
+					printf ("%3.u  %3.d  u64   ", i, pos);
+					if (!_parse_u64 (ptr, &pos, &v)) {
+						printf ("%s\n",
+							(v == UINT64_MAX)
+							? "overflow"
+							: "error");
+						goto _next_arg;
+					}
+					printf ("%" PRIu64 "\n", v);
+
+				} else {
+					++pos;
 				}
-				printf ("%u\n", v.u);
+
+			} else if (ptr[pos] == 'i') {
+				if (ptr[pos + 1] == '1'
+				    && ptr[pos + 2] == '6'
+				    && ptr[pos + 3] == ':') {
+					pos += 4;
+					int16_t v;
+					printf ("%3.u  %3.d  i16   ", i, pos);
+					if (!_parse_i16 (ptr, &pos, &v)) {
+						printf ("%s\n",
+						        (v == INT16_MAX)
+						        ? "overflow"
+						        : (v == INT16_MIN)
+						          ? "underflow"
+						          : "error");
+						goto _next_arg;
+					}
+					printf ("%" PRId16 "\n", v);
+
+				} else if (ptr[pos + 1] == '3'
+				           && ptr[pos + 2] == '2'
+				           && ptr[pos + 3] == ':') {
+					pos += 4;
+					int32_t v;
+					printf ("%3.u  %3.d  i32   ", i, pos);
+					if (!_parse_i32 (ptr, &pos, &v)) {
+						printf ("%s\n",
+						        (v == INT32_MAX)
+						        ? "overflow"
+						        : (v == INT32_MIN)
+						          ? "underflow"
+						          : "error");
+						goto _next_arg;
+					}
+					printf ("%" PRId32 "\n", v);
+
+				} else if (ptr[pos + 1] == '6'
+				           && ptr[pos + 2] == '4'
+				           && ptr[pos + 3] == ':') {
+					pos += 4;
+					int64_t v;
+					printf ("%3.u  %3.d  i64   ", i, pos);
+					if (!_parse_i64 (ptr, &pos, &v)) {
+						printf ("%s\n",
+						        (v == INT64_MAX)
+						        ? "overflow"
+						        : (v == INT64_MIN)
+						          ? "underflow"
+						          : "error");
+						goto _next_arg;
+					}
+					printf ("%" PRId64 "\n", v);
+
+				} else {
+					++pos;
+				}
+
 			} else {
-				printf ("int32 ");
-				if (!_parse_i32 (ptr, &pos, &v.i)) {
-					printf ("%s\n",
-					        (v.i == INT32_MAX)
-					        ? "overflow"
-					        : (v.i == INT32_MIN)
-					          ? "underflow"
-					          : "error");
-					return EXIT_FAILURE;
-				}
-				printf ("%d\n", v.i);
+				++pos;
 			}
 		}
+
+	_next_arg:
+		++i;
 	}
+
 	return EXIT_SUCCESS;
 }
