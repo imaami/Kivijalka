81c08a9d668c18f3d39398a8f6a5770cdf03105f
Juuso Alasuutari <juuso.alasuutari@koneetkiertoon.fi>
2015-08-19 13:54:28 +0300

    Adding config file (to be installed in /etc/kivijalka.conf).

--- server/kivijalka.sh
+++ server/kivijalka.sh
@@ -1,44 +1,125 @@
 #!/bin/bash
 
+CFGFILE="/etc/kivijalka.conf"
+
+unset KIVIJALKA_ADDR KIVIJALKA_PORT KIVIJALKA_RUNPATH KIVIJALKA_CACHEPATH \
+      KIVIJALKA_DISPLAY_WIDTH KIVIJALKA_DISPLAY_HEIGHT KIVIJALKA_STOP \
+      KIVIJALKA_FONT_SIZE KIVIJALKA_ROW_HEIGHT
+
+if [[ -f "$CFGFILE" ]]
+then
+  . "$CFGFILE"
+fi
+
+if [[ -n $KIVIJALKA_ADDR ]]
+then
+  ADDR="$KIVIJALKA_ADDR"
+else
+  ADDR='127.0.0.1'
+fi
+
+if [[ -n $KIVIJALKA_PORT ]]
+then
+  PORT="$KIVIJALKA_PORT"
+else
+  PORT='8001'
+fi
+
+if [[ -n $KIVIJALKA_RUNPATH ]]
+then
+  RUNPATH="$KIVIJALKA_RUNPATH"
+else
+  RUNPATH='/run/kivijalka'
+fi
+
+if [[ -n $KIVIJALKA_CACHEPATH ]]
+then
+  CACHEPATH="$KIVIJALKA_CACHEPATH"
+else
+  CACHEPATH='/var/cache/kivijalka'
+fi
+
+if [[ -n $KIVIJALKA_DISPLAY_WIDTH ]] &&
+   (( KIVIJALKA_DISPLAY_WIDTH > 0 ))
+then
+  PX_W="$KIVIJALKA_DISPLAY_WIDTH"
+else
+  PX_W=''
+fi
+
+if [[ -n $KIVIJALKA_DISPLAY_HEIGHT ]] &&
+   (( KIVIJALKA_DISPLAY_HEIGHT > 0 ))
+then
+  PX_H="$KIVIJALKA_DISPLAY_HEIGHT"
+else
+  PX_H=''
+fi
+
+if [[ -n $KIVIJALKA_STOP ]]
+then
+  STOP="$KIVIJALKA_STOP"
+else
+  STOP='1'
+fi
+
+if [[ -n $KIVIJALKA_FONT_SIZE ]]
+then
+  FONT_SIZE="$KIVIJALKA_FONT_SIZE"
+else
+  FONT_SIZE=''
+fi
+
+if [[ -n $KIVIJALKA_ROW_HEIGHT ]]
+then
+  ROW_HEIGHT="$KIVIJALKA_ROW_HEIGHT"
+else
+  ROW_HEIGHT=''
+fi
+
 BASE="http://lissu.tampere.fi/monitor.php?stop="
-STOP="0516"
-FONT_SIZE=50
-ROW_HEIGHT=55
-PARAMS="&font_size=$FONT_SIZE&row_height=$ROW_HEIGHT"
-URL="$BASE$STOP$PARAMS"
 OUTPATH="/dev/shm/kivijalka"
-BANPATH="/usr/share/kivijalka/banners"
 CAP="cap-$STOP.png"
 OUT="out-$STOP.png"
-PIDFILE="$OUTPATH/lock-$STOP.pid"
-ADDR='127.0.0.1'
-PORT='8001'
+PIDFILE="$RUNPATH/lock-$STOP.pid"
 
 mkdir -p "$OUTPATH"
-mkdir -p "$BANPATH"
-
-# get resolution
-#x=$(xrandr -q|egrep "^Screen 0"|sed -r 's|(^.*current )([1-9][0-9]*) x ([1-9][0-9]*),.*$|\2 \3|')
-#PX_W="${x%% *}"
-#PX_H="${x##* }"
-PX_W=1080
-PX_H=1920
-
-# defaults if we didn't find a resolution
-if [[ -z $PX_W ]]
-then
-  PX_W=1280
-fi
-if [[ -z $PX_H ]]
-then
-  PX_H=1024
-fi
+mkdir -p "$CACHEPATH"
+mkdir -p "$RUNPATH"
 
 if [[ -f "$PIDFILE" ]]
 then
   kill -9 $(< "$PIDFILE") &>/dev/null
   rm -f "$PIDFILE"
 fi
+
+(( FONT_SIZE >= 30 && FONT_SIZE <= 50 )) || FONT_SIZE=''
+(( ROW_HEIGHT >= 20 && ROW_HEIGHT <= 80 )) || ROW_HEIGHT=''
+(( PX_W > 0 )) || PX_W=1280
+(( PX_H > 0 )) || PX_H=1024
+
+# source stream parameters
+if [[ -f "$RUNPATH/s" ]]
+then
+  TMP="$(< "$RUNPATH/s")"
+  TMP2="$(grep -o ',[0-9]*' <<< "$TMP")"
+  TMP2="${TMP2##*,}"
+  TMP="${TMP%%,*}"
+  (( TMP == FONT_SIZE || TMP < 30 || TMP > 50 )) || (( FONT_SIZE=TMP ))
+  (( TMP2 == ROW_HEIGHT || TMP2 < 20 || TMP2 > 80 )) || (( ROW_HEIGHT=TMP2 ))
+fi
+
+# display parameters
+if [[ -f "$RUNPATH/d" ]]
+then
+  TMP="$(< "$RUNPATH/d")"
+  TMP2="$(grep -o 'x[0-9]*' <<< "$TMP")"
+  TMP2="${TMP2##*x}"
+  TMP="${TMP%%x*}"
+  (( TMP == PX_W || TMP < 1 )) || (( PX_W=TMP ))
+  (( TMP2 == PX_H || TMP2 < 1 )) || (( PX_H=TMP2 ))
+fi
+
+URL="$BASE$STOP${FONT_SIZE:+&font_size=$FONT_SIZE}${ROW_HEIGHT:+&row_height=$ROW_HEIGHT}"
 
 cutycapt --url="$URL" \
   --private-browsing=on \
@@ -58,6 +139,33 @@ fi
   echo $BASHPID > "$PIDFILE"
   while true
   do
+    # source stream parameters
+    if [[ -f "$RUNPATH/s" ]]
+    then
+      TMP="$(< "$RUNPATH/s")"
+      TMP2="$(grep -o ',[0-9]*' <<< "$TMP")"
+      TMP2="${TMP2##*,}"
+      TMP="${TMP%%,*}"
+      (( TMP == FONT_SIZE || TMP < 30 || TMP > 50 )) || (( FONT_SIZE=TMP ))
+      (( TMP2 == ROW_HEIGHT || TMP2 < 20 || TMP2 > 80 )) || (( ROW_HEIGHT=TMP2 ))
+    fi
+
+    # display parameters
+    if [[ -f "$RUNPATH/d" ]]
+    then
+      TMP="$(< "$RUNPATH/d")"
+      TMP2="$(grep -o 'x[0-9]*' <<< "$TMP")"
+      TMP2="${TMP2##*x}"
+      TMP="${TMP%%x*}"
+      (( TMP == PX_W || TMP < 1 )) || (( PX_W=TMP ))
+      (( TMP2 == PX_H || TMP2 < 1 )) || (( PX_H=TMP2 ))
+    fi
+
+    (( FONT_SIZE >= 30 && FONT_SIZE <= 50 )) || FONT_SIZE=''
+    (( ROW_HEIGHT >= 20 && ROW_HEIGHT <= 80 )) || ROW_HEIGHT=''
+
+    URL="$BASE$STOP${FONT_SIZE:+&font_size=$FONT_SIZE}${ROW_HEIGHT:+&row_height=$ROW_HEIGHT}"
+
     if cutycapt --url="$URL" \
          --private-browsing=on \
          --javascript=off \
@@ -74,7 +182,7 @@ fi
 ) &>/dev/null &
 
 (
-  wsserver -c "$OUTPATH/$CAP" -o "$OUTPATH/$OUT" -b "$BANPATH" \
+  wsserver -c "$OUTPATH/$CAP" -o "$OUTPATH/$OUT" -b "$CACHEPATH" \
            -g "${PX_W}x${PX_H}" -a "$ADDR" -p "$PORT"
 ) &>/dev/null &
 
